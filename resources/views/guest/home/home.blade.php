ab4fc83222c9c511db7015a28e64ac58ba2d3397
Sabrina <sabrina@hooijmans.mobi>
2020-06-30 08:48:52 +0200

    Added bootstrap

--- resources/views/guest/home/home.blade.php
+++ resources/views/guest/home/home.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.guest.layout')
 
 @section('content')
-    <h3>
-        Homepage
-    </h3>
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <h3>Homepage</h3>
+        </div>
+    </div>
+</div>
 @endsection
