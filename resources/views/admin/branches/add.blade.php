c16e7e0193d9c6beca06098cb9dc60c415f068dc
Sabrina <sabrina@hooijmans.mobi>
2020-07-01 18:27:19 +0200

    Added basic form validation; added links, added openinghours view

--- resources/views/admin/branches/add.blade.php
+++ resources/views/admin/branches/add.blade.php
@@ -12,12 +12,15 @@
         <div class="col-sm-6 offset-sm-3 pt-md-3">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ url()->current() }}" method="POST">
+                    <form action="{{ url()->current() }}" method="POST" class="needs-validation" novalidate>
                         @csrf
                         <div class="form-row">
                             <div class="col form-group">
                                 <label for="name">Naam:</label>
-                                <input type="text" name="name" class="form-control" id="name">
+                                <input type="text" name="name" class="form-control" id="name" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldige naam in.
+                                </div>
                             </div>
                             <div class="col-3 form-group">
                                 <label for="status">Status:</label>
@@ -31,36 +34,54 @@
                         <div class="form-row">
                             <div class="col form-group">
                                 <label for="address">Adres:</label>
-                                <input type="text" name="address" class="form-control" id="address"">
+                                <input type="text" name="address" class="form-control" id="address" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldig adres in.
+                                </div>
                             </div>
                             
                             <div class="col-3 form-group">
                                 <label for="house_number">Huisnummer:</label>
-                                <input type="text" name="house_number" class="form-control" id="house_number"">
+                                <input type="text" name="house_number" class="form-control" id="house_number" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldig huisnummer in.
+                                </div>
                             </div>
                         </div>
 
                         <div class="form-row">
                             <div class="col form-group">
                                 <label for="zipcode">Postcode:</label>
-                                <input type="text" name="zipcode" class="form-control" id="zipcode"">
+                                <input type="text" name="zipcode" class="form-control" id="zipcode" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldige postcode in.
+                                </div>
                             </div>
 
                             <div class="col form-group">
                                 <label for="city">Plaats:</label>
-                                <input type="text" name="city" class="form-control" id="city"">
+                                <input type="text" name="city" class="form-control" id="city" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldige plaats in.
+                                </div>
                             </div>
                         </div>
                         
                         <div class="form-row">
                             <div class="col form-group">
                                 <label for="phonenumber">Telefoonnummer:</label>
-                                <input type="text" name="phonenumber" class="form-control" id="phonenumber"">
+                                <input type="text" name="phonenumber" class="form-control" id="phonenumber" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldig telefoonnummer in.
+                                </div>
                             </div>
 
                             <div class="col form-group">
                                 <label for="email">Email:</label>
-                                <input type="email" name="email" class="form-control" id="email"">
+                                <input type="email" name="email" class="form-control" id="email" required>
+                                <div class="invalid-feedback">
+                                    Vul a.u.b. een geldig email in.
+                                </div>
                             </div>
                         </div>
 
@@ -122,7 +143,7 @@
                             <div class="col form-group">
                                 <label for="max_distance_delivery">Max. afstand bezorgen:</label>
                                 <div class="input-group mb-2">
-                                    <input type="number" name="max_distance_delivery_km" class="form-control" id="max_distance_delivery">
+                                    <input type="number" min="0" name="max_distance_delivery_km" class="form-control" id="max_distance_delivery" required>
                                     <div class="input-group-prepend">
                                         <div class="input-group-text">km</div>
                                     </div>
@@ -135,7 +156,7 @@
                                     <div class="input-group-prepend">
                                         <div class="input-group-text">€</div>
                                     </div>
-                                    <input type="number" step="0.5" name="delivery_costs" class="form-control" id="delivery_costs">
+                                    <input type="number" step="0.5" min="0" name="delivery_costs" class="form-control" id="delivery_costs" required>
                                 </div>
                             </div>
                         </div>
@@ -147,7 +168,7 @@
                                     <div class="input-group-prepend">
                                         <div class="input-group-text">€</div>
                                     </div>
-                                    <input type="number" name="delivery_free_at" class="form-control" id="delivery_free_at">
+                                    <input type="number" step="0.5" min="0" name="delivery_free_at" class="form-control" id="delivery_free_at" required>
                                 </div>
                             </div>
 
@@ -157,7 +178,7 @@
                                     <div class="input-group-prepend">
                                         <div class="input-group-text">€</div>
                                     </div>
-                                    <input type="number" step="0.5" name="delivery_min_amount" class="form-control" id="delivery_min_amount">
+                                    <input type="number" step="0.5" min="0" name="delivery_min_amount" class="form-control" id="delivery_min_amount" required>
                                 </div>
                             </div>
                         </div>
@@ -173,3 +194,24 @@
 </div>
 
 @endsection
+
+<script>
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+</script>
