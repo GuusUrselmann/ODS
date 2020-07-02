f417bdcaf067980f0bbff288e440ab23f261cdc4
Sabrina <sabrina@hooijmans.mobi>
2020-07-02 10:44:24 +0200

    Put JS form validation in a separate file

--- resources/views/admin/couponcodes/edit.blade.php
+++ resources/views/admin/couponcodes/edit.blade.php
@@ -95,27 +95,6 @@
         </div>
     </div>
 </div>
-
-<script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-</script>
 @stop
 
 @section('css')
@@ -123,5 +102,5 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script src="{{ url('/js/utilities/form-validation.js') }}"></script>
 @stop
