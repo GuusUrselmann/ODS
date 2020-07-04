b1015aeb1c028f71a88a2cfe118c44cd6a581627
Sabrina <sabrina@hooijmans.mobi>
2020-07-04 11:46:12 +0200

    Added model and connected to controller

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Category;
+use App\Branch;
 
 class CategoriesController extends Controller
 {
@@ -18,57 +20,71 @@ class CategoriesController extends Controller
 
     public function overview()
     {
-        $categories = [
-            [
-                'id' => 1,
-                'name' => 'Voorgerechten',
-                'status' => "active",
-                'created_at' => "02/07/2020",
-                'updated_at' => null,
-                'deleted_at' => null,
-                'webshop_position' => 1,
-            ],
-            [
-                'id' => 2,
-                'name' => 'Hoofdgerechten',
-                'status' => "active",
-                'created_at' => "02/07/2020",
-                'updated_at' => null,
-                'deleted_at' => null,
-                'webshop_position' => 2,
-            ],
-            [
-                'id' => 3,
-                'name' => 'Nagerechten',
-                'status' => "active",
-                'created_at' => "02/07/2020",
-                'updated_at' => null,
-                'deleted_at' => null,
-                'webshop_position' => 3,
-            ],
-        ];
+        $categories = Category::all();
+        
+        // $categories = [
+        //     [
+        //         'id' => 1,
+        //         'name' => 'Voorgerechten',
+        //         'status' => "active",
+        //         'created_at' => "02/07/2020",
+        //         'updated_at' => null,
+        //         'deleted_at' => null,
+        //         'webshop_position' => 1,
+        //     ],
+        //     [
+        //         'id' => 2,
+        //         'name' => 'Hoofdgerechten',
+        //         'status' => "active",
+        //         'created_at' => "02/07/2020",
+        //         'updated_at' => null,
+        //         'deleted_at' => null,
+        //         'webshop_position' => 2,
+        //     ],
+        //     [
+        //         'id' => 3,
+        //         'name' => 'Nagerechten',
+        //         'status' => "active",
+        //         'created_at' => "02/07/2020",
+        //         'updated_at' => null,
+        //         'deleted_at' => null,
+        //         'webshop_position' => 3,
+        //     ],
+        // ];
 
         return view('admin.categories.overview', ['categories' => $categories]);
     }
 
     public function add()
     {
-        return view('admin.categories.add');
+        // $branches = Branch::all();
+
+        $branches = [
+            [
+                'id' => 1,
+                'name' => "De Grot"
+            ]
+        ];
+
+        return view('admin.categories.add', ['branches' => $branches]);
     }
 
     public function edit()
     {
-        $category = [
-            'id' => 1,
-            'name' => 'Voorgerechten',
-            'status' => "inactive",
-            'created_at' => "02/07/2020",
-            'updated_at' => null,
-            'deleted_at' => null,
-            'webshop_position' => 1,
+        $category = Category::find(request('id'));
+        // $branches = Branch::all();
+        
+        $branches = [
+            [
+                'id' => 1,
+                'name' => "De Grot"
+            ]
         ];
 
-        return view('admin.categories.edit', [ 'category' => $category]);
+        return view('admin.categories.edit', [ 
+            'category' => $category,
+            'branches' => $branches    
+        ]);
     }
 
     /*
@@ -76,6 +92,17 @@ class CategoriesController extends Controller
     */
     public function save()
     {
+        $highestPosition = Category::where('status', 'active')->max('webshop_position');
+
+        $category = new Category();
+
+        $category->name = request('name');
+        $category->status = request('status');
+        $category->branch_id = request('branch_id');
+        $category->webshop_position = $highestPosition + 1;
+
+        $category->save();
+
         return redirect(url('/admin/categorieen'));
     }
 
@@ -84,6 +111,14 @@ class CategoriesController extends Controller
     */
     public function update()
     {
+        $category = Category::find(request('id'));
+
+        $category->name = request('name');
+        $category->status = request('status');
+        $category->branch_id = request('branch_id');
+
+        $category->save();
+
         return redirect(url('/admin/categorieen'));
     }
 
