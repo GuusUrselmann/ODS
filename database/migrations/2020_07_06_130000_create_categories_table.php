c1674a87885119975f9482a61d00daf1307357ec
Sabrina <sabrina@hooijmans.mobi>
2020-07-06 09:34:59 +0200

    Added migrations (ordered), added user in seeder

---
+++ database/migrations/2020_07_06_130000_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigInteger('branch_id')->unsigned();
+            $table->integer('webshop_position');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('branch_id')->references('id')->on('branches')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,15 +14,30 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-            'username' => 'Admin',
-            'email' => 'guusurselmann@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'first_name' => 'Guus',
-            'last_name' => 'Urselmann',
-            'user_type_id' => '1',
-            'remember_token' => Str::random(10)
-        ]);
+        App\User::create(
+            [
+                'username' => 'Admin',
+                'email' => 'guusurselmann@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'first_name' => 'Guus',
+                'last_name' => 'Urselmann',
+                'user_type_id' => '1',
+                'remember_token' => Str::random(10)
+            ]
+        );
+
+        App\User::create(
+            [
+                'username' => 'Admin Sabrina',
+                'email' => 'sabrina.jh20@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'first_name' => 'Sabrina',
+                'last_name' => 'Hooijmans',
+                'user_type_id' => '1',
+                'remember_token' => Str::random(10)
+            ]
+        );
     }
 }
