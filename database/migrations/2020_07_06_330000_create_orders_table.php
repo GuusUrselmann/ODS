98614394082d57288e114eb93a3f7414aa66d2ff
Sabrina <sabrina@hooijmans.mobi>
2020-07-07 10:01:25 +0200

    Added customer/order table, changed order so that migrations work

---
+++ database/migrations/2020_07_06_330000_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_id')->unsigned();
+            $table->datetime('order_datetime')->useCurrent();
+            $table->enum('status', ['in_process', 'on_the_way', 'delivered', 'canceled']);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
